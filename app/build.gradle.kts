f1079934d89679122d761c823434680f2912f628
lxy <1293635288@qq.com>
2026-07-14 23:19:49 +0800

    chore: bump to 5.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,9 @@ android {
         applicationId = "com.eteditor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 84
+        versionCode = 85
 
-        versionName = "5.5.1"
+        versionName = "5.5.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
